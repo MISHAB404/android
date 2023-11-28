b8b9c8398a27c3e1063886b3128312f660bcd59d
Mishab <113359139+MISHAB404@users.noreply.github.com>
2023-11-28 12:30:24 +0530

    Add files via upload

---
+++ 7.Image-Toggle/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.imgtoggle;
+
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    int image[]={R.mipmap.img1,R.mipmap.img2};
+    int currentindex=0;
+    Button btn;
+    ImageView imv;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn=findViewById(R.id.button);
+        imv=findViewById(R.id.imageView);
+
+    }
+
+
+    public void click(View view) {
+        currentindex();
+    }
+
+    private void currentindex() {
+        switch(currentindex){
+            case 0:imv.setImageResource(image[1]);
+                currentindex=1;
+                break;
+            case 1:imv.setImageResource(image[0]);
+                currentindex=0;
+                break;
+
+        }
+    }
+}
